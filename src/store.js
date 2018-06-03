39b9003e9a2c168ca83da56df7a106d569f779bd
hernani <hernani.luiz@gmail.com>
2018-06-03 18:50:24 -0300

    Store start

---
+++ src/store.js
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { YOUTUBE, TICKETMASTER } from './Api/http-commom';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+	  youtubeResult: {},
+	  tickeMasterResult: {},
+  },
+  mutations: {
+	  GET_YOUTUBE_RESULT: (state, payload) => {
+    	  state.youtubeResult = payload;
+	  },
+	  GET_TICKE_MASTER_RESULT: (state, payload) => {
+		  state.tickeMasterResult = payload;
+	  },
+  },
+  actions: {
+    SEARCH_YOUTUBE: ({ commit }, payload) =>
+      new Promise((resolve, reject) => {
+		  YOUTUBE.get('/search', {
+			  params: {
+				  q: payload,
+			  },
+		  });
+      }).then((response) => {
+        commit('GET_YOUTUBE_RESULT', response);
+      }).catch((erro) => {
+		  console.error(erro);
+	  }),
+  },
+  SEARCH_TICKET_MASTER: ({ commit }, payload) => new Promise((resolve, reject) => {
+    TICKETMASTER.get('?events.json', {
+      params: {
+        keyword: payload,
+      },
+    }).then((response) => {
+      commit('GET_TICKE_MASTER_RESULT', response);
+    }).catch((error) => {
+      console.error(erro);
+    });
+  }),
+});
