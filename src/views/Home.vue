3b806a46edd294e1be932b22aa1492cf3fc0c383
hernani <hernani.luiz@gmail.com>
2018-06-03 19:36:02 -0300

    Add components in home view

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<main class="home">
+		<search/>
+		<result/>
+	</main>
+</template>
+
+<script>
+import search from '@/components/search.vue';
+import result from '@/components/result.vue';
+
+export default {
+  name: 'Home',
+  components: {
+	  search,
+	  result,
+  },
+};
+</script>
+
