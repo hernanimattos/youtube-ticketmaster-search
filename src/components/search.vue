1b122ae30e6d01665ab3a9cb7c5af5fd3f6f4171
hernani <hernani.luiz@gmail.com>
2018-06-03 19:34:52 -0300

    Add Search component

---
+++ src/components/search.vue
@@ -0,0 +1,33 @@
+<template>
+	<main>
+		<form @submit.prevent="searchTerms" class="form-search" :class="{'top':(resultSearchCount > 0)}">
+			<input type="text" name="" id="" placeholder="Ex: r2" v-model="search">
+			<input type="submit" value="Buscar">
+		</form>
+		<span class="result-total" v-if="this.resultSearchCount > 0">Total: {{this.resultSearchCount}}</span>
+	</main>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+	  return {
+		  search: '',
+	  };
+  },
+  computed: {
+    resultSearchCount() {
+      return '';
+    },
+  },
+  methods: {
+    searchTerms() {
+      this.$root.$emit('search', this.search.replace(/\s/g, '+'));
+      this.$store.dispatch('SEARCH_YOUTUBE', this.search.replace(/\s/g, '+'));
+      this.search = '';
+    },
+  },
+};
+</script>
+
