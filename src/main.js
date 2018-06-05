d233160a84bbe2c101ab892ed3d8d356f6175c89
hernani <hernani.luiz@gmail.com>
2018-06-05 00:17:48 -0300

    Create filter to date

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router';
 import store from './store';
 import './assets/styles/reset.css';
 
+Vue.filter('formatDate', (value) => {
+  const theDate = new Date(value);
+  const day = theDate.getDay();
+  const month = theDate.getMonth() + 1;
+  const year = theDate.getFullYear();
+  return `${day}/${month}/${year}`;
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
