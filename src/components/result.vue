c7b91fef3085470efca3ccf6fe17265c1d5af3e7
hernani <hernani.luiz@gmail.com>
2018-06-05 00:17:35 -0300

    Return and list events

--- src/components/result.vue
+++ src/components/result.vue
@@ -1,20 +1,36 @@
 <template>
 	<section class="result">
-		<span v-if="resultYoutubeSearchItems && resultYoutubeSearchItems.length == 0">No results to search term</span>
-		<ul>
-			<li class="result-item" v-for="(result, index) in resultYoutubeSearchItems" :key="result[index]">
-				<details>
-					<summary><h2> {{ result.snippet.title }}</h2></summary>
-					<div class="result-item--infos">
-						<img :src="result.snippet.thumbnails.medium.url" alt="" @click="showVideoModal(result.id.videoId)">
-						<div>
-							datas
+		<article>
+			<span v-if="resultYoutubeSearchItems && resultYoutubeSearchItems.length == 0">No results to search term</span>
+			<ul>
+				<li class="result-item" v-for="(result, index) in resultYoutubeSearchItems" :key="result[index]">
+					<details>
+						<summary><h2> {{ result.snippet.title }}</h2></summary>
+						<div class="result-item--infos">
+							<img :src="result.snippet.thumbnails.high.url" alt="" @click="showVideoModal(result.id.videoId)" class="result-item-video--thumbnail">
+							<div class="result-item--channel">
+								<h3><strong>Channel: </strong>{{result.snippet.channelTitle}}</h3>
+								<p><strong>Description: </strong>{{result.snippet.description}}</p>
+								<a :href="`https://www.youtube.com/channel/${result.snippet.channelId}`">See more videos from channel</a>
+							</div>
 						</div>
+					</details>
+				</li>
+			</ul>
+		</article>
+		<article v-if="resultTicketMasterEvents">
+				<span v-if="resultTicketMasterPage && resultTicketMasterPage.totalElements == 0">No results to search term</span>
+			<h2>EVENTS</h2>
+			<details v-if="resultTicketMasterEvents != undefined" v-for="event in resultTicketMasterEvents.events" :key="event.id">
+				<summary>
+					<h2>{{event.name}}</h2>
+				</summary>
+					<div>
+						<h3>Start Sales: {{ event.sales.public.startDateTime | formatDate }}</h3>
+						<h3>End Sales: {{ event.sales.public.endDateTime | formatDate }}</h3>
 					</div>
-
-				</details>
-			</li>
-		</ul>
+			</details>
+		</article>
 			<modal :videoId="idVideo" :show-data="active" @close="active = $event">
 		</modal>
 	</section>
@@ -38,8 +54,11 @@ export default {
     resultYoutubeSearchItems() {
       return this.$store.state.youtubeResult.items;
     },
-    resultTicketMaster() {
-      return this.$store.state.tickeMasterResult;
+    resultTicketMasterEvents() {
+      return this.$store.state.tickeMasterResult._embedded;
+    },
+	 resultTicketMasterPage() {
+      return this.$store.state.tickeMasterResult.page;
     },
   },
   methods: {
